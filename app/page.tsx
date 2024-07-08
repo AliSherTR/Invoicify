d327b000e14e3da8bff6c655c0861b0dd6e9c784
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-07-08 12:51:44 +0500

    theme: dark || white, UI: add an invoice

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,84 @@
-import Image from "next/image";
+import AddInvoice from "@/components/AddInvoice";
+import Invoice from "@/components/Invoice";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <>
+            <div className="flex px-3 py-2 m-auto mt-10 justify-between max-w-3xl items-center">
+                <div>
+                    <h2 className="dark:text-white text-2xl font-semibold mb-5">
+                        All Invoices
+                    </h2>
+                    <p className="dark:text-white text-[#888eb0] text-[0.75rem]">
+                        There are total 7 invoices
+                    </p>
+                </div>
+                <div className=" flex items-center gap-4">
+                    <Select>
+                        <SelectTrigger className="outline-none border-none w-[150px] focus:outline-purple-700 dark:text-white dark:bg-transparent bg-transparent">
+                            <SelectValue placeholder="Filter by status" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="draft">Draft</SelectItem>
+                            <SelectItem value="pending">Pending</SelectItem>
+                            <SelectItem value="paid">Paid</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <div className=" ">
+                        <Sheet>
+                            <SheetTrigger className=" me-2 px-2 gap-3 py-2 text-xs flex items-center bg-blue-500 rounded-[50px] text-white font-bold">
+                                <span className="rounded-full bg-white flex items-center justify-center p-3">
+                                    <svg
+                                        width="11"
+                                        height="11"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
+                                            fill="#7C5DFA"
+                                            fillRule="nonzero"
+                                        ></path>
+                                    </svg>
+                                </span>
+                                <span className="me-2">New Invoice</span>
+                            </SheetTrigger>
+                            <SheetContent
+                                side="left"
+                                className="w-[800px] dark:bg-[#141625]"
+                            >
+                                <SheetTitle>New Invoice</SheetTitle>
+                                <AddInvoice />
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+            </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            <div className=" mt-5 max-w-3xl m-auto">
+                <Invoice status="pending" />
+                <Invoice status="paid" />
+                <Invoice status="draft" />
+                <Invoice status="pending" />
+                <Invoice status="paid" />
+                <Invoice status="draft" />
+                <Invoice status="pending" />
+            </div>
+        </>
+    );
 }
---
+++ components/AddInvoice.tsx
@@ -0,0 +1,315 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+export default function AddInvoice() {
+    return (
+        <form className=" ">
+            <p className=" text-[#7c5dfa] text-sm font-bold mb-3 mt-5">
+                Bill from
+            </p>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    Street Address
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            <div className=" flex justify-between items-center gap-3 mb-5">
+                <div>
+                    <label
+                        htmlFor="city"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        City
+                    </label>
+                    <input
+                        type="text"
+                        id="city"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="post-code"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Post Code
+                    </label>
+                    <input
+                        type="text"
+                        id="post-code"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="country"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Country
+                    </label>
+                    <input
+                        type="text"
+                        id="country"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+            </div>
+
+            <p className=" text-[#7c5dfa] text-sm font-bold mb-3 mt-5">
+                Bill To
+            </p>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    {`Client's`} Name
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    {`Client's`} Email
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    Street Address
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            <div className=" flex justify-between items-center gap-3 mb-5">
+                <div>
+                    <label
+                        htmlFor="city"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        City
+                    </label>
+                    <input
+                        type="text"
+                        id="city"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="post-code"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Post Code
+                    </label>
+                    <input
+                        type="text"
+                        id="post-code"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="country"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Country
+                    </label>
+                    <input
+                        type="text"
+                        id="country"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+            </div>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    Invoice Date
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    Payment Terms
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            <div className=" mb-5">
+                <label
+                    htmlFor="street-address"
+                    className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                >
+                    Project Description
+                </label>
+                <input
+                    type="text"
+                    id="street-address"
+                    className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                />
+            </div>
+
+            {/* ITEMS LIST */}
+            <p className=" text-[#777f98] text-lg font-bold mb-3 mt-5">
+                Items List
+            </p>
+
+            <div className=" flex justify-between items-center gap-3 mb-5">
+                <div>
+                    <label
+                        htmlFor="city"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Item Name:
+                    </label>
+                    <input
+                        type="text"
+                        id="city"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="post-code"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Quantity:
+                    </label>
+                    <input
+                        type="text"
+                        id="post-code"
+                        className=" w-full py-3 px-2 border border-gray-300  dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="country"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Price
+                    </label>
+                    <input
+                        type="text"
+                        id="country"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor="country"
+                        className="text-[#7e88c3] text-xs block mb-3 dark:text-white"
+                    >
+                        Total
+                    </label>
+                    <input
+                        type="text"
+                        id="country"
+                        className=" w-full py-3 px-2 border border-gray-300 dark:bg-[#1e2139] dark:border-[#1e2139]"
+                        disabled
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor="country"
+                        className="text-[#7e88c3] text-xs block mb-3 opacity-0"
+                    >
+                        1234
+                    </label>
+
+                    <button>
+                        <svg
+                            width="13"
+                            height="16"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className=" hover:fill-red-600 ms-2 transition-all duration-150 ease-in-out dark:fill-white dark:hover:fill-red-600"
+                        >
+                            <path
+                                d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+                                fillRule="nonzero"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <Button
+                variant="outline"
+                className=" text-center w-full flex items-center text-[#7e88c3] font-extrabold text-sm"
+            >
+                {" "}
+                <span className="me-3">+</span> Add New Item
+            </Button>
+
+            <div className=" flex justify-between items-center w-full mt-5 mb-5">
+                <Button
+                    variant="outline"
+                    className=" text-center  text-[#7e88c3]  text-sm"
+                >
+                    {" "}
+                    Discard
+                </Button>
+                <div>
+                    <Button
+                        variant="outline"
+                        className=" text-center rounded-[20px] py-4 me-3  text-sm"
+                    >
+                        Save as Draft
+                    </Button>
+                    <Button className=" text-center rounded-[20px] py-4  text-sm">
+                        Save & Send
+                    </Button>
+                </div>
+            </div>
+        </form>
+    );
+}
