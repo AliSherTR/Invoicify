6232d664690751b9616eeb90357824e1c1094475
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-07-11 16:56:25 +0500

    + client side form validation

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,23 @@
 "use server";
-export async function createInvoice(data: FormData) {
-    console.log(data.get("city"));
+
+export type State = {
+    status: "success";
+    message: string;
+} | null;
+export async function createInvoice(
+    prevState: State | null,
+    data: FormData
+): Promise<State> {
     console.log("Server Component");
+    const formDataObject: { [key: string]: string } = {};
+    for (const [key, value] of Object.entries(Object.fromEntries(data))) {
+        formDataObject[key] = value.toString();
+    }
+
+    // Log all key-value pairs in the formDataObject
+    for (const [key, value] of Object.entries(formDataObject)) {
+        console.log(`${key}: ${value}`);
+    }
+
+    return { status: "success", message: "Invoice created successfully" };
 }
